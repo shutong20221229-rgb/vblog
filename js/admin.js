5323046e024434a86b67807b58fb9c40e774dda7
shutong20221229 <shutong20221229@shutong20221229deMacBook.local>
2026-04-06 06:51:11 -0700

    Revert back to vblog repository

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,5 @@
 const GITHUB_OWNER = 'shutong20221229-rgb';
-const GITHUB_REPO = 'shutong20221229-rgb.github.io';
+const GITHUB_REPO = 'vblog';
 const BRANCH = 'main';
 const SCRIPT_VERSION = '1.1';
 
